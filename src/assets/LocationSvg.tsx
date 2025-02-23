6b841177a3cfad3e0398918c3c057d04dd273226
mrskg0438 <gaudshwetkamal0438@gmail.com>
2025-02-23 19:27:11 +0530

    build error

--- src/assets/LocationSvg.tsx
+++ src/assets/LocationSvg.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const LocationSvg = () => {
     return (
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" stroke-width="1.5">
